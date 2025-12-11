4560630a63d84c0d70d645be5c0af8b0b2d47bc4
Arjav Jain <arjavjain0703@gmail.com>
2025-12-11 17:04:56 +0530

    run git command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::process::Command;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Git::status().await?;
+    Ok(())
+}
+
+struct Git {}
+
+impl Git {
+    async fn status() -> Result<()> {
+        let output = Command::new("git").arg("status").output()?;
+        dbg!(output);
+        Ok(())
+    }
 }
